0dcddf71bab5b76df5b879a698ce3ae42c4e28e2
ncuyen <uyen.nguyencong@gmail.com>
2018-11-11 05:09:10 +0700

    Setup Frontend side

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './lib/components/Index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
+
+module.exports = config;
+
